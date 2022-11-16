4bfd7298a373e1aa01f0d30e0d9e8d952f1a0bea
Lokfor <pedro.rodrma@gmail.com>
2022-11-16 14:24:36 -0300

    Profile adiciondo

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = '';
+    const url = 'https://raw.githubusercontent.com/Lokfor/Portfolio/master/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
